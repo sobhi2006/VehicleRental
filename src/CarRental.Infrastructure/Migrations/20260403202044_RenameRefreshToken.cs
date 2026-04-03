a01e42a94d7d084a4845ba70172ba2ef5defdc8b
sobhi2006 <sobhihazouri2006@gmail.com>
2026-04-03 23:21:24 +0300

    Rename refreshToken Columns

---
+++ src/CarRental.Infrastructure/Migrations/20260403202044_RenameRefreshToken.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRental.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameRefreshToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TokenHash",
+                table: "RefreshTokens",
+                newName: "RefreshTokenHash");
+
+            migrationBuilder.RenameColumn(
+                name: "ReplacedByTokenHash",
+                table: "RefreshTokens",
+                newName: "ReplacedByRefreshTokenHash");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RefreshTokens_TokenHash",
+                table: "RefreshTokens",
+                newName: "IX_RefreshTokens_RefreshTokenHash");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RevokedAccessTokens_AspNetUsers_UserId",
+                table: "RevokedAccessTokens",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RevokedAccessTokens_AspNetUsers_UserId",
+                table: "RevokedAccessTokens");
+
+            migrationBuilder.RenameColumn(
+                name: "ReplacedByRefreshTokenHash",
+                table: "RefreshTokens",
+                newName: "ReplacedByTokenHash");
+
+            migrationBuilder.RenameColumn(
+                name: "RefreshTokenHash",
+                table: "RefreshTokens",
+                newName: "TokenHash");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RefreshTokens_RefreshTokenHash",
+                table: "RefreshTokens",
+                newName: "IX_RefreshTokens_TokenHash");
+        }
+    }
+}
